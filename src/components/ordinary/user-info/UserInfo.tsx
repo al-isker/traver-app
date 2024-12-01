94a400df464b37eb78b85f1a6abc18eae4146d06
Alisker <alisker.babaev@pay-go.ru>
2024-12-01 13:41:47 +0300

    ranf

--- src/components/ordinary/user-info/UserInfo.tsx
+++ src/components/ordinary/user-info/UserInfo.tsx
@@ -1,9 +1,9 @@
 import { UserInfoItem } from './UserInfoItem';
 
 export const UserInfo = () => (
-	<div className='flex gap-x-3'>
-		<UserInfoItem number='Аксакал' caption='ранг' />
+	<div className='grid-row-2 grid grid-cols-2 gap-3'>
 		<UserInfoItem number={65} caption='пройдено км' />
-		{/* <UserInfoItem number={724} caption='Коины' /> */}
+		<UserInfoItem number={724} caption='Коины' />
+		<UserInfoItem className='col-span-2' number='Аксакал' caption='ранг' />
 	</div>
 );
--- src/components/ordinary/user-info/UserInfoItem.tsx
+++ src/components/ordinary/user-info/UserInfoItem.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
 
+import clsx from 'clsx';
+
 import { Container } from '@/components/reused/container/Container';
 
 interface Props {
+	className?: string;
 	number: ReactNode;
 	caption: ReactNode;
 }
 
-export const UserInfoItem = ({ number, caption }: Props) => {
+export const UserInfoItem = ({ number, caption, className }: Props) => {
 	return (
-		<div className='w-full rounded bg-white py-3 shadow-sm'>
+		<div className={clsx('w-full rounded bg-white py-2 shadow-sm', className)}>
 			<Container className='flex flex-col items-center'>
-				<span className='text-2xl font-bold text-black/75'>{number}</span>
+				<span className='text-xl font-bold text-black/75'>{number}</span>
 				<span className='text-nowrap text-sm font-medium tracking-wide text-black/60'>
 					{caption}
 				</span>
