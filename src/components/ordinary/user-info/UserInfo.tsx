09a6405943ee3fbcb51ef474cc72c157733b326f
Alisker <alisker.babaev@pay-go.ru>
2024-12-01 13:34:50 +0300

    ввв

--- src/components/ordinary/user-info/UserInfo.tsx
+++ src/components/ordinary/user-info/UserInfo.tsx
@@ -2,8 +2,8 @@ import { UserInfoItem } from './UserInfoItem';
 
 export const UserInfo = () => (
 	<div className='flex gap-x-3'>
-		<UserInfoItem number={7} caption='Ранг' />
-		<UserInfoItem number={65} caption='Пройдено км' />
+		<UserInfoItem number='Аксакал' caption='ранг' />
+		<UserInfoItem number={65} caption='пройдено км' />
 		{/* <UserInfoItem number={724} caption='Коины' /> */}
 	</div>
 );
