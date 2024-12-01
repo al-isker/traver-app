6f308fe195c79fe61a1b624dfa63cd004cbade9e
Alisker <alisker.babaev@pay-go.ru>
2024-12-01 13:46:02 +0300

    tripcoins

--- src/components/ordinary/user-info/UserInfo.tsx
+++ src/components/ordinary/user-info/UserInfo.tsx
@@ -3,7 +3,7 @@ import { UserInfoItem } from './UserInfoItem';
 export const UserInfo = () => (
 	<div className='grid-row-2 grid grid-cols-2 gap-3'>
 		<UserInfoItem number={65} caption='пройдено км' />
-		<UserInfoItem number={724} caption='Коины' />
+		<UserInfoItem number={724} caption='TripCoins' />
 		<UserInfoItem className='col-span-2' number='Аксакал' caption='ранг' />
 	</div>
 );
